33b93d3f5c3bd9c4827371a38f6e2d788787902b
Pseudocoder Kim <digression99@gmail.com>
2019-09-26 17:12:42 +0900

    Update setup to download fzf and projectionist

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+
+
 cd ~
 cp ~/dot-files/.zshrc ~/.zshrc
 cp ~/dot-files/.vimrc ~/.vim/vimrc
@@ -17,4 +19,13 @@ then
 	cd $VIMCONFIG/pack/minpac/opt
 	git clone https://github.com/k-takata/minpac.git
 fi
+
+# installing fuzzy finder.
+cd $VIMCONFIG/pack/bundle/start
+git clone https://github.com/junegunn/fzf
+$VIMCONFIG/pack/bundle/start/fzf/install --bin
+
+cd $VIMCONFIG/pack/bundle/start
+git clone https://github.com/tpope/vim-projectionist.git
+
 
