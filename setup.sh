c396bc7e93be7f8105e3c8fb0c83f074cfef05dd
Pseudocoder Kim <digression99@gmail.com>
2019-09-28 11:13:01 +0900

    Update setup

--- setup.sh
+++ setup.sh
@@ -2,48 +2,36 @@ cd ~
 cp ~/dot-files/.zshrc ~/.zshrc
 cp ~/dot-files/.vimrc ~/.vim/vimrc
 
-INSTALL_MINPAC=false
-INSTALL_FZF=false
-INSTALL_PROJECTIONIST=false
+
+INSTALL_PACKAGES=false
 INSTALL_PROGRAMS=false
 
 while getopts ifp opt;
 do
 	case $opt in
-		i) INSTALL_MINPAC=true ;;
-		f) INSTALL_FZF=true ;;
-		p) INSTALL_PROJECTIONIST=true ;;
-		c) INSTALL_PROGRAMS=true ;;
+		i) INSTALL_PACKAGES=true ;;
+		b) INSTALL_PROGRAMS=true ;;
 	esac
 done
 
-if [[ "$INSTALL_MINPAC" == true ]]
+if [[ "$INSTALL_PACKAGES" == true ]]
 then
 	mkdir -p $VIMCONFIG/pack/minpac/opt
 	cd $VIMCONFIG/pack/minpac/opt
 	git clone https://github.com/k-takata/minpac.git
-fi
 
-# installing fuzzy finder.
-if [[ "$INSTALL_FZF" == true ]]
-then
 	cd $VIMCONFIG/pack/bundle/start
 	git clone https://github.com/junegunn/fzf
 	$VIMCONFIG/pack/bundle/start/fzf/install --bin
-	brew install ripgrep
-fi
 
-# installing fuzzy finder.
-if [[ "$INSTALL_PROJECTIONIST" == true ]]
-then
-	cd $VIMCONFIG/pack/bundle/start
 	git clone https://github.com/tpope/vim-projectionist.git
+	git clone https://github.com/tpope/vim-dispatch.git
 fi
 
 # install programs
 if [[ "$INSTALL_PROGRAMS" == true ]]
 then
+	brew install ripgrep
 	brew install ctags
 fi
-
 
